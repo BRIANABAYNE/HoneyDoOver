5d9bd49cb4a653b0af5d13c897d95d695b0bf0ec
Briana Bayne <brianabayne@brianas-imac.lan>
2023-08-19 17:35:11 -0600

    Working on TableView.

--- HoneyDoOver/HoneyDoOver/HoneyDo/HoneyDoTableViewController.swift
+++ HoneyDoOver/HoneyDoOver/HoneyDo/HoneyDoTableViewController.swift
@@ -9,47 +9,60 @@ import UIKit
 
 class HoneyDoTableViewController: UITableViewController {
 
+    
+// MARK: - Outlets
+    
+    @IBOutlet weak var honeDoTextField: UITextField!
+    
+    
+// MARK: - Properties
+    
+var viewModel: HoneyDoViewModel!
+    
+    
+    // MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+  viewModel = HoneyDoViewModel()
+        
     }
+    
+    
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func honeyDoButtonTapped(_ sender: Any) {
+        
+    }
+    
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+
+ override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     return viewModel.honeyDos.count
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "honeyCell", for: indexPath) as? HoneyDoTableViewCell
+        else { return UITableViewCell() }
+        
+        let honeyDo = viewModel.honeyDos.[indexPath.row]
+        cell.del
 
         return cell
     }
-    */
+ 
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -59,22 +72,22 @@ class HoneyDoTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+ 
 
-    /*
+
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    */
 
-    /*
+
+ 
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+ 
 
     /*
     // MARK: - Navigation
@@ -87,3 +100,5 @@ class HoneyDoTableViewController: UITableViewController {
     */
 
 }
+
+
